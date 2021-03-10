64767bd09150edafd6c1ba90ad9b378011e7c6d0
ria0617 <ria0617@daum.net>
2021-03-10 16:42:36 +0900

    로그인 및 회원가입기능 상세주석 달았어요

--- src/main/java/com/board/controller/UserLoginController.java
+++ src/main/java/com/board/controller/UserLoginController.java
@@ -1,6 +1,9 @@
 package com.board.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -11,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.WebUtils;
 
 import com.board.domain.LoginDTO;
 import com.board.domain.UserVO;
@@ -40,15 +44,41 @@ public class UserLoginController {
 	public void loginPost(LoginDTO loginDTO, HttpSession httpSession, Model model) throws Exception{
 		logger.info("post login");
 		
+		//login에서 받은 데이터(아이디, 비밀번호) 중에서 
+		//아이디를 통해 select한 회원 정보를 변수 userVO에 담는다
 		UserVO userVO = userService.login(loginDTO);
 		
+		//만역 userVO가 null 또는 BCrypt.checkpw()를 통해 맞지 않으면 메서드 종료
 		if (userVO ==null || !BCrypt.checkpw(loginDTO.getPw(), userVO.getPw())) {
 			return;
 		}
-		
+		//비밀번호가 일치하면 model에 userVO를 user란 변수로 저장한다
 		model.addAttribute("user", userVO);
-
 	}
+	
+	//로그아웃 처리
+//	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+//	public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession) throws Exception{
+//		logger.info("get logout");
+//		
+//		Object object = httpSession.getAttribute("login");
+//		if (object != null) {
+//			UserVO userVO = (userVO) object;
+//			httpSession.removeAttribute("login");
+//			httpSession.invalidate();
+//			Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
+//			if (loginCookie != null) {
+//				loginCookie.setPath("/");
+//				loginCookie.setMaxAge(0);
+//				response.addCookie(loginCookie);
+//				userService.keepLogin(userVO.getUserId(), "none", new Date());
+//			}
+//		}
+//		
+//		return "/member/logout";
+//	}
+	
+	
 }
 
 
--- src/main/java/com/board/controller/commons/interceptor/LoginInterceptor.java
+++ src/main/java/com/board/controller/commons/interceptor/LoginInterceptor.java
@@ -14,29 +14,41 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 	private static final String LOGIN = "login";
 	private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 	
-	//세션 저장
+	//HttpSession과 관련된 모든 설정을 처리하는 곳
+	
+	//httpSession에 컨트롤러에서 저장한 user를 저장
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		
+		//세션을 가져온다 (가져올 세션이 없으면 생성)
 		HttpSession httpSession = request.getSession();
+		
+		//model에 데이터를 저장 후 view(jsp페이지)에서 데이터 접근이 가능
 		ModelMap modelMap = modelAndView.getModelMap();
+		
+		//userVO = 로그인 폼에 입력한 정보
 		Object userVO = modelMap.get("user");
 		
+		//가져온 데이터가 null이 아니면 로그인 성공
 		if (userVO != null) {
 			logger.info("로그인 성공");
+			//LOGIN으로 session을 바인딩 한다
 			httpSession.setAttribute(LOGIN, userVO);
 			response.sendRedirect("/");
 		}
 	}
 	
-	//세션 제거
+	//기존 로그인 정보가 있을 겨우 초기화
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		
+		//세션을 가져온다 (가져올 세션이 없으면 생성)
 		HttpSession httpSession = request.getSession();
-		//기존 로그인 정보 제거
+		
+		//LOGIN 값이 null이 아니면 제거해라
 		if (httpSession.getAttribute(LOGIN) != null) {
 			logger.info("로그인 정보가 사라졌습니다");
+			//LOGIN 제거하기 = null값으로 만들기
 			httpSession.removeAttribute(LOGIN);
 		}
 		
